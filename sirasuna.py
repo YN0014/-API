76da5cb5f84f23363920b88a4f073293e78dfb5f
N_Yuta <guguru0014@gmail.com>
2017-10-28 17:17:43 +0900

    Update sirasuna.py

--- sirasuna.py
+++ sirasuna.py
@@ -2,18 +2,28 @@ import urllib.request
 import codecs
 import re
 import datetime
+import glob
+
 
 def html_get():
     url = "http://shirasunaryou.sakura.ne.jp/cgi-bin/shirasuna/kondate/index.cgi?display=sp"
-    title = "kondate_html.txt"
+    title = "kondate_html_1now.txt"
     urllib.request.urlretrieve(url, "{0}".format(title))
+    url = "http://shirasunaryou.sakura.ne.jp/cgi-bin/shirasuna/kondate/index.cgi?next=1&display=sp"
+    title = "kondate_html_2next.txt"
+    urllib.request.urlretrieve(url, "{0}".format(title))
+    f = open('kondate_html.txt', 'w')
+    for name in glob.glob('kondate_html_*.txt'):
+        for line in codecs.open(name, 'r', 'shift_jis'):
+            f.write(line[:-1])
+    f.close()
     kondate_get()
 
 
 def kondate_get():
     kondate = []
 
-    f = codecs.open('kondate_html.txt', 'r', 'shift_jis')
+    f = codecs.open('kondate_html.txt', 'r', 'utf-8', 'ignore')
     file_1 = open('kondate_1.txt', 'w')
     file_2 = open('kondate_2.txt', 'w')
     file_3 = open('kondate_3.txt', 'w')
@@ -35,10 +45,11 @@ def kondate_get():
             line = "\n" + line
             kondate.append(line)
 
-    today = datetime.datetime.today()
     flag = 0
     for line in kondate:
-        tmp = str(today.month) + "/" + str(today.day + flag)
+        today = datetime.datetime.today()
+        today += datetime.timedelta(days=flag)
+        tmp = str(today.month) + "/" + str(today.day)
         if tmp in line:
             flag += 1
         if flag == 0:
